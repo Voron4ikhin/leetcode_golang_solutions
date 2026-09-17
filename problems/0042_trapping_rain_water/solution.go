cd7cf0b16d42aabb7829e2861f78673c342efc26
i.voronchikhin <i.voronchikhin@krediska.ru>
2026-09-17 22:26:49 +0300

    refactor problem 42

--- problems/0042_trapping_rain_water/solution.go
+++ problems/0042_trapping_rain_water/solution.go
@@ -1,5 +1,22 @@
 package _042_trapping_rain_water
 
 func Trap(height []int) int {
-	return 0
+	l := 0
+	r := len(height) - 1
+	maxL := height[l]
+	maxR := height[r]
+	water := 0
+
+	for l < r {
+		if maxR > maxL {
+			l++
+			maxL = max(maxL, height[l])
+			water += maxL - height[l]
+		} else {
+			r--
+			maxR = max(maxR, height[r])
+			water += maxR - height[r]
+		}
+	}
+	return water
 }
