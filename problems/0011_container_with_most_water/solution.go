a25e003374ee2a07d930def2d200c697c278502c
i.voronchikhin <i.voronchikhin@krediska.ru>
2026-09-17 21:18:40 +0300

    refactor problem 11

--- problems/0011_container_with_most_water/solution.go
+++ problems/0011_container_with_most_water/solution.go
@@ -2,26 +2,25 @@ package _001_two_sum
 
 func MaxArea(height []int) int {
 	var result int
-	left := 0
-	right := len(height) - 1
-
-	for left != right {
-		minHeight := 0
-		if height[left] > height[right] {
-			minHeight = height[right]
-		} else {
-			minHeight = height[left]
+	left, right := 0, len(height)-1
+	for left < right {
+		areaPre := area(left, right, height)
+		if areaPre > result {
+			result = areaPre
 		}
-		square := minHeight * (right - left)
-		if square > result {
-			result = square
-		}
-		if height[left] > height[right] {
-			right--
-		} else {
+		if height[left] < height[right] {
 			left++
+			continue
+		}
+		if height[left] >= height[right] {
+			right--
+			continue
 		}
 	}
 
 	return result
+}
+
+func area(l, r int, height []int) int {
+	return (r - l) * min(height[l], height[r])
 }
